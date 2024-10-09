8b206e4fba01e587e5a44b66695c7c7c37de4994
Anfang Liu <lafhhh@outlook.com>
2024-10-09 18:42:28 +0300

    Add about page with title, content, and imageList

--- frontend/src/components/pages/About.js
+++ frontend/src/components/pages/About.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './Styles.css';
+import AboutImageList from './AboutImageList';
 
 export const About = () => {
   return (
-    <div>
-      <h1 className="page-title"> About </h1>
-      <p className="textStyle"> Some information about this application.</p>
+    <div className="aboutContainer">
+      <div className="aboutInfo">
+        <div className="aboutTitle">
+          <h1>About </h1>
+        </div>
+        <div className="aboutContent">
+          <p>
+            Cooking should be enjoyable and accessible to everyone, regardless of their measurement
+            preferences.
+          </p>
+          <p>
+            Our web app is designed with a single focus: to help you convert measurement types for
+            recipes effortlessly.
+          </p>
+          <p>
+            Our team is passionate about cooking and believes that good food brings people together.
+            We aim to bridge the gap between different measurement systems, ensuring that you can
+            find comfort in your favorite recipes, no matter where you are in the world.
+          </p>
+        </div>
+      </div>
+      <div className='aboutImageList'>
+        <AboutImageList />
+      </div>
     </div>
   );
 };
