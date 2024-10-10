7ea22641c87b24bc7d47ef57f417a89a8b0bc2f5
Anfang Liu <lafhhh@outlook.com>
2024-10-10 15:51:59 +0300

    1. Add link to MyRecipeBook. 2. Add home, add, info, and cog icon to buttons on the top bar. 3. Add copyright button to bottom bar. 4. Add background color to topbar, bottom bar, and whole background

--- frontend/src/components/common/Footer.js
+++ frontend/src/components/common/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Footer.css';
+import CopyrightOutlinedIcon from '@mui/icons-material/CopyrightOutlined';
+import { Stack, Typography } from '@mui/material';
 
 export const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} MyRecipeBook </p>
+      <Stack alignItems="center" justifyContent="center" direction="row" gap={1}>
+        <CopyrightOutlinedIcon />
+        <Typography variant="body1"> {new Date().getFullYear()} MyRecipeBook</Typography>
+      </Stack>
     </footer>
   );
 };
