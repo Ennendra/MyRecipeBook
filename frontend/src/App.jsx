8e5b67e6d5de2a08b45175e5a2ce999bfb517c64
Anastasiia Vdovenko <anastasiia.vdovenko@learnit.fi>
2024-10-11 11:25:07 +0300

    Added the ability show popup with route navigate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { default as React } from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Outlet,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import './App.css';
 import { Footer } from './components/common/Footer';
 import { Header } from './components/common/Header';
@@ -9,27 +16,34 @@ import { RecipeEditor } from './components/pages/RecipeEditor';
 import { Settings } from './components/pages/Settings';
 import { ViewRecipe } from './components/pages/ViewRecipe';
 
-function App() {
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      {/* Redirect from "/" to "/home" */}
+      <Route index element={<Navigate to="/home" />} />
+      <Route path="home/:searchPattern?" element={<Home />} />
+      <Route path="addNewRecipe" element={<RecipeEditor />} />
+      <Route path="about" element={<About />} />
+      <Route path="settings" element={<Settings />} />
+      <Route path="viewRecipe/:id" element={<ViewRecipe />} />
+    </Route>
+  )
+);
+
+function Layout() {
   return (
     <>
-      <Router>
-        <Header />
-        <hr className="separator" />
-        <Routes>
-          {/* Set default path. */}
-          <Route path="/" element={<Navigate to="/home" />} />
-
-          <Route path="/home/:searchPattern?" element={<Home />} />
-          <Route path="/addNewRecipe" element={<RecipeEditor />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/viewRecipe/:id" element={<ViewRecipe />} />
-        </Routes>
-      </Router>
-
+      <Header />
+      <hr className="separator" />
+      {/* The <Outlet> is where the nested route elements are rendered */}
+      <Outlet />
       <Footer />
     </>
   );
+}
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
