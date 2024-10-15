040ebceb9e76f78cb4856019392ee3c0521c2423
Ian Marchant <ian.lachlan.marchant@gmail.com>
2024-10-15 12:49:16 +0300

    Added page refresh functionality to homepage buttons if already on home page

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -10,13 +10,24 @@ import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 
 export const Header = () => {
   const navigate = useNavigate();
+
+  const homeNavigation = () => {
+    const currentPath = window.location.pathname;
+    if (currentPath === '/home') {
+      window.location.reload(false);
+    }
+    else {
+      navigate('/home')
+    }
+  };
+
   return (
     <header className="header">
-      <Button className="header-title"  onClick={() => navigate('/home')} variant="text" color='success'>
+      <Button className="header-title"  onClick={homeNavigation} variant="text" color='success'>
         <h1>MyRecipeBook</h1>
         </Button>
       <Stack direction="row" spacing={2}>
-        <Button onClick={() => navigate('/home')} startIcon={<HomeOutlinedIcon />} variant='contained' color='success'>Home</Button>
+        <Button onClick={homeNavigation} startIcon={<HomeOutlinedIcon />} variant='contained' color='success'>Home</Button>
         <Button onClick={() => navigate('/addNewRecipe')} startIcon={<AddOutlinedIcon />} variant='contained' color='success'>Add recipe</Button>
         <Button onClick={() => navigate('/about')} startIcon={<InfoOutlinedIcon />} variant='contained' color='success'>About</Button>
         <Button onClick={() => navigate('/settings')} startIcon={<SettingsOutlinedIcon />} variant='contained' color='success'>Settings</Button>
